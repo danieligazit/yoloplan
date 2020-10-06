ae9004dddf4ca70475e4e07b23e7b0e81e469ea3
Or Gany <or@interay.co>
2020-10-06 14:21:26 +0300

    Adding custom foramt to the logger

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,20 +1,32 @@
 use {
-    std::{io::Result},
     api::main_config,
+    std::{io::{Result, Write}},
+    chrono::Local,
     actix_web::{
-        App, HttpServer, middleware
-    }
+        App, HttpServer, middleware,
+    },
 };
 
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+    configure_logger();
 
     HttpServer::new(|| {
         App::new()
-            .wrap(middleware::Logger::default())
+            .wrap(middleware::Logger::new("%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
             .configure(main_config)
     }).bind("127.0.0.1:8080")?.run().await
+}
+
+/// Set configurations for the app logger
+fn configure_logger() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+
+    env_logger::builder()
+        .format(|formatter, record| {
+            writeln!(formatter, "{} [{}] - {}", Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                     record.level(), record.args())
+        })
+        .init();
 }
